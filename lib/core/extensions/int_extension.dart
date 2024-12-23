7de8fb0e81d0032a6072817b3903bd83637a7d04
Hovsep <Hovsep.Hayrapetyan@sksoldev.com>
2024-12-23 06:15:53 +0400

    [gps_walk_test]: add timer logic and pause functionality

--- lib/core/extensions/int_extension.dart
+++ lib/core/extensions/int_extension.dart
@@ -2,7 +2,7 @@ extension IntExtension on int {
   String get formatTime {
     final Duration duration = Duration(seconds: this);
     final int minutes = duration.inMinutes;
-    final int seconds = duration.inSeconds % 60;  // Get the seconds part only
+    final int seconds = duration.inSeconds % 60;
     return "${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
 }
